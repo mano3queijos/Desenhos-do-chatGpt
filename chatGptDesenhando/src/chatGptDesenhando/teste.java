c479b158c875d333c803ff6161121b67c233af3b
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-05-10 02:58:49 -0300

    desenhos

---
+++ chatGptDesenhando/src/chatGptDesenhando/teste.java
@@ -0,0 +1,59 @@
+package chatGptDesenhando;
+
+public class teste {
+
+	public static void main(String[] args) {
+		// eu pedi pro chat gpt desenhar uma peira, uma maçã mordida e um cachorro e foi
+		// isso que ele fex kkk
+
+		System.out.println("Desenhos do chatGpt \n");
+
+		// isso era pra ser uma peira
+		System.out.println("   ,#####,");
+		System.out.println("  #_   _#");
+		System.out.println(" //     \\\\");
+		System.out.println("|\\\"\\.../\"/|");
+		System.out.println("| \\\\_\\_// |");
+		System.out.println("|  \"\"\"\"\"  |");
+		System.out.println(" \\_______/");
+		System.out.println("    / \\");
+		System.out.println("   /   \\");
+		System.out.println("  /     \\");
+		System.out.println(" (   P   )");
+		System.out.println("  \\     /");
+		System.out.println("   =====");
+		System.out.println("\n");
+		
+		// isso era pra ser uma maçã mordida?????
+		System.out.println("      __ ");
+		System.out.println("    .'  `\\ ");
+		System.out.println("  .'      \\ ");
+		System.out.println(" /    /   |");
+		System.out.println("|    |    |");
+		System.out.println("|    |    |");
+		System.out.println("|    |    |");
+		System.out.println("|    |    |");
+		System.out.println("|    |    |");
+		System.out.println("|    |    |");
+		System.out.println("|    |    |");
+		System.out.println("|    |    |");
+		System.out.println("|    |    |");
+		System.out.println("|    |    |");
+		System.out.println("|    |    |");
+		System.out.println("|   /     |");
+		System.out.println(" \\        |");
+		System.out.println("  \\       |");
+		System.out.println("   `\\_    |");
+		System.out.println("      `\\__|");
+		
+		// isso até que se parece com um cachorro, um cachorro de madamekkkkkkk
+		System.out.println("     /\\_/\\" );
+		System.out.println("    / o o \\" );
+		System.out.println("   (   \"   )" );
+		System.out.println("    \\~(*)~/");
+		System.out.println("     - ^ -" );
+
+
+	}
+
+}
